182d8483b2f4016f2a4f372f684ead3f9625fefa
noleynik29 <noleynik29@gmail.com>
2025-12-03 18:25:46 +0200

    Initial commit

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -1,9 +1,13 @@
 package mate.academy.model;
-
-import jakarta.persistence.*;
-
 import java.time.LocalDateTime;
 import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "orders")
